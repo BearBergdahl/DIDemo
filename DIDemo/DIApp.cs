0fb736fe8cd150c7376fa8fd00f3a29548e3eaed
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2023-11-14 11:43:29 +0100

    Add project files.

---
+++ DIDemo/DIApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIDemo
+{
+    public interface IApp
+    {
+        void Print();
+    }
+
+    public class DIAppProd : IApp
+    {
+        public void Print() 
+        {
+            Console.WriteLine("Print ImplementationProd!");
+        }
+    }
+
+    public class  DIAppTest : IApp
+    {
+        public void Print()
+        {
+            Console.WriteLine("Print ImplmentationTest");
+        }
+    }
+}
---
+++ DIDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using DIDemo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        EnvVar envVar = EnvVar.Test;
+        if (envVar == EnvVar.Test)
+        {
+            IApp dIAppTest = new DIAppTest();
+            DIClass implementedClass = new DIClass(dIAppTest);
+            implementedClass.PrintMethod();
+        }
+        if (envVar == EnvVar.Prod)
+        {
+            IApp diAppProd = new DIAppProd();
+            DIClass implementedClass = new DIClass(diAppProd);
+            implementedClass.PrintMethod();
+            Console.ReadLine();
+        }
+        
+    }
+
+    class DIClass
+    {
+        private IApp _app;
+        public DIClass(IApp app)
+        {
+            _app = app;
+        }
+        public void PrintMethod()
+        {
+            _app.Print();
+        }
+    }
+
+    enum EnvVar
+    {
+        Test,
+        Prod
+    }
+}
